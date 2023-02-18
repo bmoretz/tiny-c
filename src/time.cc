c9c9f054edd0c0820f9415195907aeb6b43dab02
Brandon Moretz <bmoretz@elmwoodasset.com>
2023-02-18 15:14:18 -0500

    project setup with cmake / clang, ch1/2
    project setup with cmake / clang, ch1/2

---
+++ src/time.cc
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h>
+
+int main(int argc, char *argv[]) {
+    time_t now;
+
+    time(&now);
+
+    printf("The computer think it's %ld\n", now);
+    printf("%s", ctime(&now));
+}
